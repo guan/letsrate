12cdde6eaaa6cf8a013bd4eb17275f3320d0ed40
Kuniyasu Guan <kuniyasu.guan@gmail.com>
2013-12-29 02:06:48 +0900

    Display max stars when rating_for_user

--- lib/letsrate/helpers.rb
+++ lib/letsrate/helpers.rb
@@ -8,9 +8,9 @@ module Helpers
     star = options[:star] || 5
 
     if options[:disable_after_rate] == false
-	    disable_after_rate = false
+            disable_after_rate = false
     else
-	    disable_after_rate = true
+            disable_after_rate = true
     end
 
     readonly = !(current_user && rateable_obj.can_rate?(current_user, dimension))
@@ -21,25 +21,27 @@ module Helpers
                 "data-readonly" => readonly,
                 "data-star-count" => star
   end
-  
+
   def rating_for_user(rateable_obj, rating_user, dimension = nil, options = {})
     @object = rateable_obj
     @user = rating_user
-	  @rating = Rate.find_by_rater_id_and_rateable_id_and_dimension(@user.id, @object.id, dimension)
-	  stars = @rating ? @rating.stars : 0
+          @rating = Rate.find_by_rater_id_and_rateable_id_and_dimension(@user.id, @object.id, dimension)
+          stars = @rating ? @rating.stars : 0
 
     disable_after_rate = options[:disable_after_rate] || false
-    
+
+    star = options[:star] || 5
+
     readonly=false
     if disable_after_rate
       readonly = current_user.present? ? !rateable_obj.can_rate?(current_user.id, dimension) : true
     end
- 
+
     content_tag :div, '', "data-dimension" => dimension, :class => "star", "data-rating" => stars,
                 "data-id" => rateable_obj.id, "data-classname" => rateable_obj.class.name,
                 "data-disable-after-rate" => disable_after_rate,
                 "data-readonly" => readonly,
-                "data-star-count" => stars
+                "data-star-count" => star
   end
 
 end
