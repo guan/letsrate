43cb06999192df9e8ba7af7e5da45d0cac04fad1
Kuniyasu Guan <kuniyasu.guan@gmail.com>
2013-12-30 01:14:20 +0900

    Adding readonly and use_span options

--- lib/letsrate/helpers.rb
+++ lib/letsrate/helpers.rb
@@ -1,21 +1,22 @@
 module Helpers
   def rating_for(rateable_obj, dimension=nil, options={})
-
     cached_average = rateable_obj.average dimension
-
     avg = cached_average ? cached_average.avg : 0
-
     star = options[:star] || 5
-
+    tag = options[:use_span] ? :span : :div
     if options[:disable_after_rate] == false
-            disable_after_rate = false
+      disable_after_rate = false
     else
-            disable_after_rate = true
+      disable_after_rate = true
     end
 
-    readonly = !(current_user && rateable_obj.can_rate?(current_user, dimension))
+    if !options[:readonly].nil?
+      readonly = options[:readonly]
+    else
+      readonly = !(current_user && rateable_obj.can_rate?(current_user, dimension))
+    end
 
-    content_tag :div, '', "data-dimension" => dimension, :class => "star", "data-rating" => avg,
+    content_tag tag, '', "data-dimension" => dimension, :class => "star", "data-rating" => avg,
                 "data-id" => rateable_obj.id, "data-classname" => rateable_obj.class.name,
                 "data-disable-after-rate" => disable_after_rate,
                 "data-readonly" => readonly,
@@ -29,15 +30,17 @@ module Helpers
           stars = @rating ? @rating.stars : 0
 
     disable_after_rate = options[:disable_after_rate] || false
-
+    tag = options[:use_span] ? :span : :div
     star = options[:star] || 5
+
+    tag = options[:use_span] ? :span : :div
 
     readonly=false
     if disable_after_rate
       readonly = current_user.present? ? !rateable_obj.can_rate?(current_user.id, dimension) : true
     end
 
-    content_tag :div, '', "data-dimension" => dimension, :class => "star", "data-rating" => stars,
+    content_tag tag, '', "data-dimension" => dimension, :class => "star", "data-rating" => stars,
                 "data-id" => rateable_obj.id, "data-classname" => rateable_obj.class.name,
                 "data-disable-after-rate" => disable_after_rate,
                 "data-readonly" => readonly,
